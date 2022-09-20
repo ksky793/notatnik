88b54f18a2105171b47a2688dd6efa77b73fdfc1
ksky793 <k.sky793@gmail.com>
2022-09-20 05:12:12 +0200

    created Searchbar  component without a functionality

---
+++ src/components/ui/searchbar/Searchbar.js
@@ -0,0 +1,20 @@
+import { BsSearch } from 'react-icons/bs';
+import './Searchbar.css';
+const Searchbar = () => {
+	return (
+		<div className='searchbar'>
+			<div className='searchbar-left-side'>
+				<BsSearch className='ic ic-search' />
+			</div>
+			<div className='searchbar-right-side'>
+				<input
+					type='text'
+					className='searchbar-input'
+					placeholder='Search...'
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default Searchbar;
